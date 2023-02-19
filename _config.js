44f40456319b66fb05974ddd72ec2cdb7665b46c
MikeDee <mwangimike15@yahoo.com>
2023-02-20 02:34:33 +0300

    add text jenkins file

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://mikedee:S3Y8Kp7zbSWv6u4y@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://mikedee:S3Y8Kp7zbSWv6u4y@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    production: 'mongodb+srv://mikedee:S3Y8Kp7zbSWv6u4y@moringa-devops.3mlwhea.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://mikedee:S3Y8Kp7zbSWv6u4y@moringa-devops.3mlwhea.mongodb.net/?retryWrites=true&w=majority',
     test: 'mongodb+srv://mikedee:S3Y8Kp7zbSWv6u4y@moringa-devops.3mlwhea.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
